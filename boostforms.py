8d993a085f4af2b81321695e5cfa5516548bef75
Brock Shelton <brocks@Brocks-MBP.fios-router.home>
2018-08-21 16:53:51 -0400

    login form

--- boostforms.py
+++ boostforms.py
@@ -2,18 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SelectField
 from wtforms.validators import InputRequired, Email, Length
 
-leagues = ['Bronze 5', 'Bronze 4', 'Bronze 3', 'Bronze 2', 'Bronze 1']
-silver = ['Silver 5', 'Silver 4', 'Silver 3', 'Silver 2', 'Silver 1']
-gold = ['Gold 5', 'Gold 4', 'Gold 3', 'Gold 2', 'Gold 1']
-zipped = zip(leagues, silver, gold)
+leagues = [('Bronze 5', 'Bronze 5'), ('Bronze 4', 'Bronze 4'), ('Bronze 3','Bronze 3')]
 
-print (zipped)
 #Need a list of tuples
 
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=20)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    username = StringField('Username:', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password:', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('Remember Me')
 
 class RegisterForm(FlaskForm):
@@ -23,4 +19,4 @@ class RegisterForm(FlaskForm):
 
 
 class SoloOrderForm(FlaskForm):
-    current_rank = SelectField(label='Current Rank', choices=zipped)
+    current_rank = SelectField(label='Current Rank', choices=leagues)
